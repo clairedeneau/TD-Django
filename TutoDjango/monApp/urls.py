09806c48a24530555246d296eb7669e416d66122
clairedeneau <claire.deneau@etu.univ-orleans.fr>
2025-09-04 11:49:40 +0200

    Fin TD1

--- TutoDjango/monApp/urls.py
+++ TutoDjango/monApp/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns = [
- path("", views.home, name="home"),
+ path("home/<param>", views.home, name="home"),
+ path("home/", views.home, name="home"),
+ path("contact/", views.contact, name="contact"),
+ path("about/", views.about, name="about"),
 ]
--- TutoDjango/monApp/views.py
+++ TutoDjango/monApp/views.py
@@ -2,5 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-def home(request):
+def home(request, param = None):
+ if param :
+    return HttpResponse(f"<h1>Bonjour {param} !</h1>")
  return HttpResponse("<h1>Hello Django!</h1>")
+
+def contact(request):
+ return HttpResponse("<h1>Contact us</h1>\n<p> Ou pas</p>")
+
+def about(request):
+ return HttpResponse("<h1>About...</h1>")

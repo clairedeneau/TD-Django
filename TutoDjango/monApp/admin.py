7baee7f962a045dd361faa6585c9182ccd63c507
clairedeneau <claire.deneau@etu.univ-orleans.fr>
2025-09-08 15:28:51 +0200

    Fin ajout des rayons et de leurs produits

--- TutoDjango/monApp/admin.py
+++ TutoDjango/monApp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
+class ProduitAdmin(admin.ModelAdmin):
+ list_display = ('intituleProd', 'prixUnitaireProd')
+
+admin.site.register(Produit)
+admin.site.register(Categorie)
+admin.site.register(Statut)
+admin.site.register(Rayon)
+admin.site.register(Contenir)
 # Register your models here.
--- TutoDjango/monApp/models.py
+++ TutoDjango/monApp/models.py
@@ -11,13 +11,13 @@ class Statut(models.Model):
   idStatut = models.AutoField(primary_key=True)
   libelle = models.CharField(max_length=200)
   def __str__(self):
-    return self.nomRayon
+    return self.libelle
   
 class Produit(models.Model):
  refProd = models.AutoField(primary_key=True)
  intituleProd = models.CharField(max_length=200)
  prixUnitaireProd = models.DecimalField(max_digits=10, decimal_places=2)
- dateFabrication = models.DateField(date.today,blank=True,null=True)
+ dateFabrication = models.DateField(default=date.today,blank=True,null=True)
  # Relation CIF : chaque produit appartient à 1 catégorie (0,N côté catégorie 1,1 côté produit) →
  categorie = models.ForeignKey(Categorie, on_delete=models.CASCADE, related_name="produits",null=True, blank=True)
  statut = models.ForeignKey(Statut, on_delete=models.CASCADE, related_name="produits",null=True, blank=True)

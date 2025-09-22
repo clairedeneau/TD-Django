19deab538d1d725e5e11f481014375938f441df2
clairedeneau <claire.deneau@etu.univ-orleans.fr>
2025-09-22 15:25:48 +0200

    TD 3 page 8 fin liste produits

--- TutoDjango/monApp/urls.py
+++ TutoDjango/monApp/urls.py
@@ -1,12 +1,19 @@
 from django.urls import path
 from . import views
+from django.views.generic import *
+
 urlpatterns = [
- path("home/<param>", views.home, name="home"),
- path("home/", views.home, name="home"),
- path("contact/", views.contact, name="contact"),
- path("about/", views.about, name="about"),
- path("listeprods/", views.liste_produits, name="prod_list"),
+ #path("home/<param>", views.home, name="home"),
+ #path("home/", views.home, name="home"),
+ #path("contact/", views.contact, name="contact"),
+ #path("about/", views.about, name="about"),
+ path("listeprods/", views.ProduitListView.as_view(), name="lst_prdts"),
  path("listecats/", views.liste_categories, name="cat_list"),
  path("listestats/", views.liste_statuts, name="stat_list"),
  path("listerays/", views.liste_rayons, name="ray_list"),
+ path("home/", views.HomeView.as_view()),
+ path("home/<param>", views.HomeView.as_view()),
+ path("about/", views.AboutView.as_view()),
+ path("contact/", views.ContactView.as_view()),
+ path("produit/<pk>/",views.ProduitDetailView.as_view(), name="dtl_prdt"),
 ]
--- TutoDjango/monApp/views.py
+++ TutoDjango/monApp/views.py
@@ -1,19 +1,9 @@
 from django.shortcuts import render
 from .models import *
 from django.http import HttpResponse, Http404
+from django.views.generic import *
 
 # Create your views here.
-
-def home(request, param):
-    if request.GET and request.GET["test"]:
-        raise Http404
-    return HttpResponse("<h1>Hello " + param + " ! You're connected</h1>")
-
-def contact(request):
-    return render(request, 'monApp/contact.html')
-
-def about(request):
-    return render(request, 'monApp/about.html')
 
 def liste_produits(request):
     prdts = Produit.objects.all()
@@ -30,3 +20,57 @@ def liste_statuts(request):
 def liste_rayons(request):
     rays = Rayon.objects.all()
     return render(request, 'monApp/list_rayons.html', {'rays': rays})
+
+class HomeView(TemplateView):
+    template_name = "monApp/page_home.html"
+    def post(self, request, **kwargs):
+        return render(request, self.template_name)
+    
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        context['titreh1'] = f"Hello {self.kwargs.get('param') or "Django"} !"
+        return context
+    
+class AboutView(TemplateView):
+    template_name = "monApp/page_home.html"
+    def get_context_data(self, **kwargs):
+        context = super(AboutView, self).get_context_data(**kwargs)
+        context['titreh1'] = "About us... nothing"
+        return context
+    
+    def post(self, request, **kwargs):
+        return render(request, self.template_name)
+    
+class ContactView(TemplateView):
+    template_name = "monApp/page_home.html"
+    def get_context_data(self, **kwargs):
+        context = super(AboutView, self).get_context_data(**kwargs)
+        context['titreh1'] = "DO NOT CONTACT ME"
+        return context
+    
+    def post(self, request, **kwargs):
+        return render(request, self.template_name)
+    
+class ProduitListView(ListView):
+    model = Produit
+    template_name = "monApp/list_produits.html"
+    context_object_name = "prdts"
+
+    def get_queryset(self ) :
+        return Produit.objects.order_by("prixUnitaireProd")
+    
+    def get_context_data(self, **kwargs):
+        context = super(ProduitListView, self).get_context_data(**kwargs)
+        context['titremenu'] = "Liste de mes produits"
+        return context
+    
+class ProduitDetailView(DetailView):
+    model = Produit
+    template_name = "monApp/detail_produit.html"
+    context_object_name = "prdt"
+
+    def get_context_data(self, **kwargs):
+        context = super(ProduitDetailView, self).get_context_data(**kwargs)
+        context['titremenu'] = "Détail du produit"
+        return context
+    

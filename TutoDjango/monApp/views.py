3f8f48e59f3409056914a416931e4779b3c4f92d
clairedeneau <claire.deneau@etu.univ-orleans.fr>
2025-09-25 12:29:30 +0200

    TP3 fin logout avant màj nav

--- TutoDjango/monApp/views.py
+++ TutoDjango/monApp/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
-from .models import *
-from django.http import HttpResponse, Http404
-from django.views.generic import *
+from django.forms import BaseModelForm
+from django.shortcuts import render, redirect
+from django.http import Http404, HttpResponse, HttpResponseNotFound, JsonResponse
+from django.views.generic import  *
+#from monApp.forms import ContactUsForm, ProduitForm
+from monApp.models import *
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.core.mail import send_mail
 
 # Create your views here.
 
@@ -74,3 +80,102 @@ class ProduitDetailView(DetailView):
         context['titremenu'] = "Détail du produit"
         return context
     
+class CatListView(ListView):
+    model = Categorie
+    template_name = "monApp/list_categories.html"
+    context_object_name = "cats"
+
+    def get_queryset(self) :
+        return Categorie.objects.all()
+    
+    def get_context_data(self, **kwargs):
+        context = super(CatListView, self).get_context_data(**kwargs)
+        context['titremenu'] = "Liste des catégories"
+        return context
+    
+class CatDetailView(DetailView):
+    model = Categorie
+    template_name = "monApp/detail_categorie.html"
+    context_object_name = "cat"
+
+    def get_context_data(self, **kwargs):
+        context = super(CatDetailView, self).get_context_data(**kwargs)
+        context['titremenu'] = "Détail de la catégorie"
+        return context
+    
+class StatutListView(ListView):
+    model = Statut
+    template_name = "monApp/list_statuts.html"
+    context_object_name = "stats"
+
+    def get_queryset(self) :
+        return Statut.objects.all()
+    
+    def get_context_data(self, **kwargs):
+        context = super(StatutListView, self).get_context_data(**kwargs)
+        context['titremenu'] = "Liste des statuts"
+        return context
+    
+class StatutDetailView(DetailView):
+    model = Statut
+    template_name = "monApp/detail_statut.html"
+    context_object_name = "stat"
+
+    def get_context_data(self, **kwargs):
+        context = super(StatutDetailView, self).get_context_data(**kwargs)
+        context['titremenu'] = "Détail du statut"
+        return context
+    
+class RayonListView(ListView):
+    model = Rayon
+    template_name = "monApp/list_rayons.html"
+    context_object_name = "rays"
+
+    def get_queryset(self) :
+        return Rayon.objects.all()
+    
+    def get_context_data(self, **kwargs):
+        context = super(RayonListView, self).get_context_data(**kwargs)
+        context['titremenu'] = "Liste des rayons"
+        return context
+    
+class RayonDetailView(DetailView):
+    model = Rayon
+    template_name = "monApp/detail_rayon.html"
+    context_object_name = "ray"
+
+    def get_context_data(self, **kwargs):
+        context = super(RayonDetailView, self).get_context_data(**kwargs)
+        context['titremenu'] = "Détail du rayon"
+        return context
+    
+class ConnectView(LoginView):
+    template_name = 'monApp/page_login.html'
+    def post(self, request, **kwargs):
+        lgn = request.POST.get('username', False)
+        pswrd = request.POST.get('password', False)
+        user = authenticate(username=lgn, password=pswrd)
+        if user is not None and user.is_active:
+            login(request, user)
+            return render(request, 'monApp/page_home.html', {'param': lgn, 'message': "You're connected"})
+        else:
+            return render(request, 'monApp/page_register.html')
+        
+class RegisterView(TemplateView):
+    template_name = 'monApp/page_register.html'
+    def post(self, request, **kwargs):
+        username = request.POST.get('username', False)
+        mail = request.POST.get('mail', False)
+        password = request.POST.get('password', False)
+        user = User.objects.create_user(username, mail, password)
+        user.save()
+        if user is not None and user.is_active:
+            return render(request, 'monApp/page_login.html')
+        else:
+            return render(request, 'monApp/page_register.html')
+        
+class DisconnectView(TemplateView):
+    template_name = 'monApp/page_logout.html'
+    def get(self, request, **kwargs):
+        logout(request)
+        return render(request, self.template_name)
